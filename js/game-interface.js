eec2092ce5925f7597bd1090f29c9cd135aabf4e
Brittany Rausch and James Turello <james@>
2016-08-01 16:56:10 -0700

    add reference multi inputs in same function

--- js/game-interface.js
+++ js/game-interface.js
@@ -4,5 +4,7 @@ $(document).ready(function() {
   $("button").submit(function(event) {
     event.preventDefault();
     var cardPosition = $(this).val();
-    
-  }
+    $(".out").text("<p>" + cardPosition + "</p>");
+    console.log(cardPosition);
+  })
+});
